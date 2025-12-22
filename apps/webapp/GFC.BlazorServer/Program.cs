4bfc86bd57974d619271fc7956a3a267b0a5f198
heldersilva1972-cell <heldersilva1972@gmail.com>
2025-12-22 15:15:01 -0500

    feat: Add initial Blazor Server application setup including navigation menu and service registrations.

--- apps/webapp/GFC.BlazorServer/Program.cs
+++ apps/webapp/GFC.BlazorServer/Program.cs
@@ -106,13 +106,13 @@ public class Program
 
         // Shared services
         builder.Services.AddSingleton<BackupConfigService>();
-        // builder.Services.AddScoped<GFC.BlazorServer.Services.Camera.ICameraVerificationService, GFC.BlazorServer.Services.Camera.CameraVerificationService>();
-        // builder.Services.AddScoped<ICameraService, CameraService>();
-        // builder.Services.AddScoped<ICameraDiscoveryService, CameraDiscoveryService>();
-        // builder.Services.AddScoped<IRecordingService, RecordingService>();
-        // builder.Services.AddScoped<ICameraEventService, CameraEventService>();
-        // builder.Services.AddScoped<ICameraPermissionService, CameraPermissionService>();
-        // builder.Services.AddScoped<ICameraAuditLogService, CameraAuditLogService>();
+        builder.Services.AddScoped<GFC.BlazorServer.Services.Camera.ICameraVerificationService, GFC.BlazorServer.Services.Camera.CameraVerificationService>();
+        builder.Services.AddScoped<ICameraService, CameraService>();
+        builder.Services.AddScoped<ICameraDiscoveryService, CameraDiscoveryService>();
+        builder.Services.AddScoped<IRecordingService, RecordingService>();
+        builder.Services.AddScoped<ICameraEventService, CameraEventService>();
+        builder.Services.AddScoped<ICameraPermissionService, CameraPermissionService>();
+        builder.Services.AddScoped<ICameraAuditLogService, CameraAuditLogService>();
         builder.Services.AddSingleton<IDatabaseBackupService, GFC.BlazorServer.Services.DatabaseBackupService>();
         builder.Services.AddHostedService<GFC.BlazorServer.Services.BackupSchedulerService>();
         builder.Services.AddScoped<OverdueCalculationService>();
