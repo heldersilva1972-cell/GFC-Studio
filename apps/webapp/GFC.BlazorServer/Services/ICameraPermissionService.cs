a913558b71dddbbf5b270dc651982c22e5f905c3
heldersilva1972-cell <heldersilva1972@gmail.com>
2025-12-25 12:47:50 -0500

    refactor: Add obsolete ICameraPermissionService placeholder and deletion marker to facilitate service relocation.

--- apps/webapp/GFC.BlazorServer/Services/ICameraPermissionService.cs
+++ apps/webapp/GFC.BlazorServer/Services/ICameraPermissionService.cs
@@ -1,8 +1,13 @@
-// [NEW]
+// [OBSOLETE - DO NOT USE]
+// This interface has been moved to Services/Camera/ICameraPermissionService.cs
+// This file exists only to prevent build errors during transition
+// TODO: Delete this file after confirming no references exist
+
 using System.Threading.Tasks;
 
-namespace GFC.BlazorServer.Services
+namespace GFC.BlazorServer.Services.OBSOLETE
 {
+    [System.Obsolete("Use GFC.BlazorServer.Services.Camera.ICameraPermissionService instead")]
     public interface ICameraPermissionService
     {
         Task<bool> UserHasAnyCameraPermissionAsync(int userId);
