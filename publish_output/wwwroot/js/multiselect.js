bec6ce8935f2396048df0e2a5a2c55989a7520a7
heldersilva1972-cell <heldersilva1972@gmail.com>
2026-01-26 08:31:06 -0500

    chore: stop tracking build artifacts and publish output

--- publish_output/wwwroot/js/multiselect.js
+++
@@ -1,9 +0,0 @@
-// Helper function for Blazor multi-select elements
-window.getSelectedValues = (element) => {
-    if (!element) {
-        return [];
-    }
-    const selectElement = element;
-    const selectedOptions = Array.from(selectElement.selectedOptions);
-    return selectedOptions.map(option => option.value);
-};
--- publish_output/wwwroot/js/studio.js
+++
@@ -1,7 +0,0 @@
-// [NEW]
-function updatePreview(sections) {
-    const iframe = document.querySelector('.preview-iframe');
-    if (iframe) {
-        iframe.contentWindow.postMessage({ type: 'UPDATE_PREVIEW', sections: sections }, '*');
-    }
-}
